085fe122a79f8b1644f7e72ded496b0a98efcf9a
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2010-12-07 12:07:50 +0000

    SVN dirs: branches, tags, trunk..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@106 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

---
+++ rexseo/uninstall.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * REXseo
+ * Based on the URL-Rewrite Addon
+ * @author dh[at]gn2-netwerk[dot]de Dave Holloway
+ * @author markus.staab[at]redaxo[dot]de Markus Staab
+ * @author code[at]rexdev[dot]de jeandeluxe
+ * @package redaxo4.2
+ * @version 1.2
+ * @version svn:$Id$
+ */
+
+$REX['ADDON']['install']['rexseo'] = 0;
