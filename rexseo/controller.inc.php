3fa78210b9b49b3cea4b20451a9d22fedfda159d
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2010-12-07 12:06:41 +0000

    SVN dirs: branches, tags, trunk..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@98 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

---
+++ rexseo/controller.inc.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * REXseo
+ * Based on the URL-Rewrite Addon
+ * @author dh[at]gn2-netwerk[dot]de Dave Holloway
+ * @author markus.staab[at]redaxo[dot]de Markus Staab
+ * @author code[at]rexdev[dot]de jeandeluxe
+ * @package redaxo4.2
+ * @version 1.2
+ * @version svn:$Id$
+ */
+
+if (rex_request('rexseo_func')!="") {
+	$path = $REX['INCLUDE_PATH'].'/addons/'.$myself;
+
+	switch (rex_request('rexseo_func')) {
+		case "googlesitemap":
+			include $path.'/extensions/googlesitemap.inc.php';
+			die();
+		break;
+
+		case "robots":
+			include $path.'/extensions/robots.inc.php';
+			die();
+		break;
+
+		case "backend.css":
+			header('Content-Type:text/css');
+			echo file_get_contents($path.'/files/backend.css');
+			die();
+		break;
+
+		case "jsopenwin.gif":
+			header('Content-Type:image/gif');
+			echo file_get_contents($path.'/files/jsopenwin.gif');
+			die();
+		break;
+
+		case "jquery.highlight-3.yui.js":
+			header('Content-Type:text/javascript');
+			echo file_get_contents($path.'/files/jquery.highlight-3.yui.js');
+			die();
+		break;
+
+		default:
+		break;
+	}
+}
