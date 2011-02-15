6a4ad44530c0436342642ff1d689679baa84ee35
dh <dh@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2011-02-15 13:03:03 +0000

    1.2 entfernt.. ab jetzt nur rXXX
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@124 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

--- rexseo/config.inc.php
+++ rexseo/config.inc.php
@@ -25,9 +25,7 @@ $myself = 'rexseo';
 $Revision = '';
 $REX['ADDON'][$myself]['VERSION'] = array
 (
-'VERSION'      => 1,
-'MINORVERSION' => 2,
-'SUBVERSION'   => preg_replace('/[^0-9]/','',"$Revision$")
+  'VERSION'   => 'r'.preg_replace('/[^0-9]/','',"$Revision$")
 );
 
 // ADDON REX COMMONS
