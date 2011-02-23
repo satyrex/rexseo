72a98af2141e5703260ae1df72dda66864e770de
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2011-02-23 13:26:21 +0000

    RECODE: install routine strukturierter.. warning bei nicht vorhandener .htaccess in root raus (thx Thomas)
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@146 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

--- rexseo/install.inc.php
+++ rexseo/install.inc.php
@@ -11,88 +11,125 @@
  * @version svn:$Id$
  */
 
-// ADDON IDENTIFIER
-////////////////////////////////////////////////////////////////////////////////
-$myself      = 'rexseo';
-$myroot      = $REX['INCLUDE_PATH'].'/addons/'.$myself;
-
 // INSTALL SETTINGS
 ////////////////////////////////////////////////////////////////////////////////
-$htaccess    = rex_get_file_contents($REX['FRONTEND_PATH'].'/.htaccess');
-$triggers    = array('x-mapp-php');
-$matches     = array();
-$autoinstall = true;
+$myself            = 'rexseo';
+$myroot            = $REX['INCLUDE_PATH'].'/addons/'.$myself;
 
-// CHECK OTHER CONDITIONS
+$minimum_REX       = '4.2.1';
+$minimum_PHP       = 5;
+$required_addons   = array('textile','metainfo');
+$disable_addons    = array('url_rewrite');
+$htaccess_search   = array('x-mapp-php');
+
+$error             = array();
+
+// CHECK REDAXO VERSION
 ////////////////////////////////////////////////////////////////////////////////
-if (intval(PHP_VERSION) < 5)
+if(version_compare($REX['VERSION'].'.'.$REX['SUBVERSION'].'.'.$REX['MINORVERSION'], $minimum_REX, '<'))
 {
-  $REX['ADDON']['installmsg'][$myself] = 'Dieses Addon ben&ouml;tigt PHP 5!';
-  $REX['ADDON']['install'][$myself] = 0;
+  $error[] = 'Dieses Addon ben&ouml;tigt Redaxo Version '.$minimum_REX.' oder h&ouml;her.';
 }
-else
+
+
+// CHECK PHP VERSION
+////////////////////////////////////////////////////////////////////////////////
+if (intval(PHP_VERSION) < $minimum_PHP)
 {
-  if (OOAddon::isInstalled('url_rewrite'))
+  $error[] = 'Dieses Addon ben&ouml;tigt mind. PHP '.$minimum_PHP.'!';
+}
+
+
+// CHECK REQUIRED ADDONS
+////////////////////////////////////////////////////////////////////////////////
+foreach($required_addons as $a)
+{
+  if (!OOAddon::isInstalled($a))
   {
-    $REX['ADDON']['installmsg'][$myself] = 'Bitte <a href="index.php?page=addon&addonname=url_rewrite&uninstall=1">deinstallieren</a> Sie url_rewrite. REXseo ersetzt die Funktionalit&auml;t des url_rewrite Addons.';
+    $error[] = 'Addon "'.$a.'" ist nicht installiert.  <span style="float:right;">[ <a href="index.php?page=addon&addonname='.$a.'&install=1">'.$a.' installieren</a> ]</span>';
   }
   else
   {
-    if (!OOAddon::isInstalled('metainfo'))
+    if (!OOAddon::isAvailable($a))
     {
-      $REX['ADDON']['installmsg'][$myself] = 'Bitte <a href="index.php?page=addon&addonname=metainfo&install=1">installieren</a> Sie erst das metainfo Addon.';
-    }
-    else
-    {
-      if (!OOAddon::isAvailable('metainfo'))
-      {
-        $REX['ADDON']['installmsg'][$myself] = 'Bitte <a href="index.php?page=addon&addonname=metainfo&activate=1">aktivieren</a> Sie das metainfo Addon.';
-      }
-      else
-      {
-      // 4.3.x -> zusätzliches Feld "restrictions" (http://forum.redaxo.de/sutra80188.html#80188 -> erst ab metainfo r1871)
-      //a62_add_field(   $title,                                        $name,                           $prior, $attributes, $type, $default, $params = null,                                                                       $validate = null, $restrictions = '')
-        a62_add_field(   'RexSEO Einstellungen',                        'art_rexseo_legend',             100,    '',         12,     '',       '',                                                                                   '',               '');
-        a62_add_field(   'Manuelle URL',                                'art_rexseo_url',                101,    '',          1,     '',       '',                                                                                   '',               '');
-        a62_add_field(   'Manuelle Canonical URL',                      'art_rexseo_canonicalurl',       102,    '',          1,     '',       '',                                                                                   '',               '');
-        a62_add_field(   'Page Title',                                  'art_rexseo_title',              103,    '',          1,     '',       '',                                                                                   '',               '');
-        a62_add_field(   'Google Sitemap Priority',                     'art_rexseo_priority',           104,    '',          3,     '',       ':Automatisch berechnen|1.00:1.00|0.80:0.80|0.64:0.64|0.51:0.51|0.33:0.33|0.00:0.00', '',               '');
-
-        // INSTALL/COPY .HTACCESS FILES
-        ////////////////////////////////////////////////////////////////////////////////
-        if ($htaccess)
-        {
-          foreach($triggers as $t)
-          {
-            if(strpos($htaccess,$t)!==false)
-            {
-              $autoinstall = false;
-              $matches[] = $t;
-            }
-          }
-
-          if($autoinstall)
-          {
-            require_once $myroot.'/functions/function.rexseo_helpers.inc.php';
-            $source = $REX['INCLUDE_PATH'].'/addons/'.$myself.'/install/files/';
-            $target = $REX['HTDOCS_PATH'];
-            $result = rexseo_recursive_copy($source, $target);
-          }
-          else
-          {
-            $msg = 'RexSEO: Die original .htaccess Datei im Root Ordner enth&auml;lt potentiell kritische settings f&uuml;r den Serverbetrieb:<br>';
-            foreach($matches as $m)
-            {
-              $msg .= '<em style="margin:4px 0 4px 10px;color:black;display:inline-block;">'.$m.'</em><br />';
-            }
-            $msg .= 'Die automatische Installation der .htaccess Dateien wurde deaktiviert,<br /> Details zur manuellen Installation siehe RexSEO Hilfe.';
-            echo rex_warning($msg);
-          }
-        }
-
-        $REX['ADDON']['install'][$myself] = 1;
-      }
+      $error[] = 'Addon "'.$a.'" ist nicht aktiviert.  <span style="float:right;">[ <a href="index.php?page=addon&addonname='.$a.'&activate=1">'.$a.' aktivieren</a> ]</span>';
     }
   }
 }
+
+
+// CHECK ADDONS TO DISABLE
+////////////////////////////////////////////////////////////////////////////////
+foreach($disable_addons as $a)
+{
+  if (OOAddon::isInstalled($a) || OOAddon::isAvailable($a))
+  {
+    $error[] = 'Addon "'.$a.'" muß erst deinstalliert werden.  <span style="float:right;">[ <a href="index.php?page=addon&addonname='.$a.'&uninstall=1">'.$a.' de-installieren</a> ]</span>';
+  }
+}
+
+
+if(count($error)==0)
+{
+  // SETUP METAINFO
+  //////////////////////////////////////////////////////////////////////////////
+    // 4.3.x -> zusätzliches Feld "restrictions" (http://forum.redaxo.de/sutra80188.html#80188 -> erst ab metainfo r1871)
+    //a62_add_field( $title,                    $name,                     $prior, $attributes, $type, $default, $params = null,                                                                       $validate = null, $restrictions = '')
+      a62_add_field( 'RexSEO Einstellungen',    'art_rexseo_legend',       100,    '',         12,     '',       '',                                                                                   '',               '');
+      a62_add_field( 'Manuelle URL',            'art_rexseo_url',          101,    '',          1,     '',       '',                                                                                   '',               '');
+      a62_add_field( 'Manuelle Canonical URL',  'art_rexseo_canonicalurl', 102,    '',          1,     '',       '',                                                                                   '',               '');
+      a62_add_field( 'Page Title',              'art_rexseo_title',        103,    '',          1,     '',       '',                                                                                   '',               '');
+      a62_add_field( 'Google Sitemap Priority', 'art_rexseo_priority',     104,    '',          3,     '',       ':Automatisch berechnen|1.00:1.00|0.80:0.80|0.64:0.64|0.51:0.51|0.33:0.33|0.00:0.00', '',               '');
+  
+  
+  
+  // CHECK ROOT .HTACCESS FILE FOR CRITICAL SETTINGS
+  //////////////////////////////////////////////////////////////////////////////
+  $autoinstall  = true;
+  if (file_exists($REX['FRONTEND_PATH'].'/.htaccess'))
+  {
+    $matches  = array();
+    $htaccess = rex_get_file_contents($REX['FRONTEND_PATH'].'/.htaccess');
+  
+    foreach($htaccess_search as $needle)
+    {
+      if(strpos($htaccess,$needle)!==false)
+      {
+        $autoinstall = false;
+        $matches[] = $needle;
+      }
+    }
+  
+    if(count($matches)>0)
+    {
+      $msg = 'RexSEO: Die original .htaccess Datei im Root Ordner enth&auml;lt potentiell kritische settings f&uuml;r den Serverbetrieb:<br>';
+      foreach($matches as $m)
+      {
+        $msg .= '<em style="margin:4px 0 4px 10px;color:black;display:inline-block;">'.$m.'</em><br />';
+      }
+      $msg .= 'Die automatische Installation der .htaccess Dateien wurde deaktiviert,<br /> Details zur manuellen Installation siehe RexSEO Hilfe.';
+      echo rex_warning($msg);
+    }
+  }
+  
+  
+  // INSTALL/COPY .HTACCESS FILES
+  //////////////////////////////////////////////////////////////////////////////
+  if($autoinstall)
+  {
+    require_once $myroot.'/functions/function.rexseo_helpers.inc.php';
+    $source = $REX['INCLUDE_PATH'].'/addons/'.$myself.'/install/files/';
+    $target = $REX['HTDOCS_PATH'];
+    $result = rexseo_recursive_copy($source, $target);
+  }
+
+  $REX['ADDON']['install'][$myself] = 1;
+}
+else
+
+{
+  $REX['ADDON']['installmsg'][$myself] = '<br />'.implode($error,'<br />');
+  $REX['ADDON']['install'][$myself] = 0;
+}
+
 ?>
