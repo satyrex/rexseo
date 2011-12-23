a6dc72c73c48868b6c4729337bda67fefed1cd55
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2011-12-23 03:22:35 +0000

    RECODE: google sitemap code revamped as class.. FEATURE: new extensionpoint REXSEO_SITEMAP_ARRAY_CREATED -> passes (and receives) the sitemap as array for possible modifications..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@201 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

---
+++ rexseo/classes/class.rexseo_sitemap.inc.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * REXseo
+ * Based on the URL-Rewrite Addon
+ * @author dh[at]gn2-netwerk[dot]de Dave Holloway
+ * @author markus.staab[at]redaxo[dot]de Markus Staab
+ * @author code[at]rexdev[dot]de jeandeluxe
+ * @package redaxo4.2
+ * @version 1.3
+ * @version svn:$Id$
+ */
+
+class rexseo_sitemap
+{
+  private $host;
+  private $map_articles;
+
+
+  /**
+   * GET SITEMAP ARTICLES FROM DB
+   *
+   * @return (array) sitemap articles
+   */
+  private function get_db_articles()
+  {
+    global $REX;
+  
+    $map_articles = array();
+    $db = new rex_sql;
+    $qry = 'SELECT `id`,`clang`,`updatedate`,`path`,`art_rexseo_priority`,`art_rexseo_changefreq`
+            FROM `'.$REX['TABLE_PREFIX'].'article` 
+            WHERE `art_rexseo_sitemap_out`=\'show\' 
+            OR    (`art_rexseo_sitemap_out`=\'\' AND `status`=1);';
+    foreach($db->getDbArray($qry) as $art)
+    {
+      $map_articles[$art['id']][$art['clang']] = array('loc'        => rex_getUrl($art['id'],$art['clang']),
+                                                       'lastmod'    => date('Y-m-d\TH:i:s',$art['updatedate']).'+00:00',
+                                                       'changefreq' => self::calc_article_changefreq($art['updatedate'],$art['art_rexseo_changefreq']),
+                                                       'priority'   => self::calc_article_priority($art['id'],$art['clang'],$art['path'],$art['art_rexseo_priority'])
+                                                       );
+    }
+  
+    // EXTENSIONPOINT REXSEO_SITEMAP_ARRAY_CREATED
+    $map_articles = rex_register_extension_point('REXSEO_SITEMAP_ARRAY_CREATED',$map_articles);
+  
+    $this->map_articles = $map_articles;
+  }
+
+
+  /**
+   * CALCULATE ARTICLE PRIORITY
+   *
+   * @param $article_id           (int)     rex_article.article_id
+   * @param $clang                (int)     rex_article.clang
+   * @param $path                 (string)  rex_article.path
+   * @param $art_rexseo_priority  (float)   rex_article.art_rexseo_priority
+   *
+   * @return                      (float)   priority
+   */
+  private function calc_article_priority($article_id,$clang,$path,$art_rexseo_priority='')
+  {
+    global $REX;
+    
+    if($art_rexseo_priority!='')
+      return $art_rexseo_priority;
+  
+    if($article_id==$REX['START_ARTICLE_ID'] && $clang==$REX['START_CLANG_ID'])
+      return 1.0;
+  
+    return pow(0.8,count(explode('|',$path))-1);
+  }
+
+
+  /**
+   * CALCULATE ARTICLE CHANGEFREQ
+   *
+   * @param $updatedate            (int)    rex_article.updatedate
+   * @param $art_rexseo_changefreq (string) rex_article.art_rexseo_changefreq
+   *
+   * @return                       (string) change frequency  [never|yearly|monthly|weekly|daily|hourly|always]
+   */
+  private function calc_article_changefreq($updatedate,$art_rexseo_changefreq='')
+  {
+    if($art_rexseo_changefreq!='')
+      return $art_rexseo_changefreq;
+  
+    $age = time() - $updatedate;
+  
+    switch($age)
+    {
+      case($age<604800):
+        return 'daily';
+      case($age<2419200):
+        return 'weekly';
+      default:
+        return 'monthly';
+    }
+  }
+
+
+  /**
+   * BUILD SINGLE XML LOC FRAGMENT
+   *
+   * @param $loc        (string) article url  [including lang, excluding host]
+   * @param $lastmod    (string) article last modified date  [UNIX date]
+   * @param $changefreq (string) change frequency  [never|yearly|monthly|weekly|daily|hourly|always]
+   * @param $priority   (float)  priority  [maximum: 1.0]
+   *
+   * @return            (string) xml location fragment
+   */
+  private function xml_loc_fragment($loc,$lastmod,$changefreq,$priority)
+  {
+    $xml_loc = '  <url>'.PHP_EOL.
+    '    <loc>'.$this->host.$loc.'</loc>'.PHP_EOL.
+    '    <lastmod>'.$lastmod.'</lastmod>'.PHP_EOL.
+    '    <changefreq>'.$changefreq.'</changefreq>'.PHP_EOL.
+    '    <priority>'.number_format($priority, 2, ".", "").'</priority>'.PHP_EOL.
+    '  </url>'.PHP_EOL;
+
+    return $xml_loc;
+  }
+
+  /**
+   * CONSTRUCTOR
+   */
+  public function rexseo_sitemap()
+  {
+    global $REX;
+
+    $this->map_articles = array();
+    $this->host         = rtrim($REX['SERVER'],'/');
+
+    self::get_db_articles();
+  }
+
+
+
+
+  /**
+   * RETURN SITEMAP
+   *
+   * @param $mode       (string) output format  [currently only xml]
+   *
+   * @return            (string) xml location fragment
+   */
+  public function get($mode='xml')
+  {
+    switch($mode)
+    {
+      case'xml':
+        $xml_sitemap = '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL.
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
+
+        foreach($this->map_articles as $id=>$clangs)
+        {
+          foreach($clangs as $art)
+          {
+            $xml_sitemap .= self::xml_loc_fragment($art['loc'],$art['lastmod'],$art['changefreq'],$art['priority']);
+          }
+        }
+
+        // EXTENSIONPOINT REXSEO_SITEMAP_INJECT
+        $inject = rex_register_extension_point('REXSEO_SITEMAP_INJECT');
+        if(is_array($inject) && count($inject)>0)
+        {
+          foreach($inject as $key => $art)
+          {
+            $xml_sitemap .= self::xml_loc_fragment($art['url'],$art['lastmod'],$art['changefreq'],$art['priority']);
+          }
+        }
+
+        $xml_sitemap .= '</urlset>';
+
+        return $xml_sitemap;
+    }
+  }
+
+
+}
+
+?>
--- rexseo/install.inc.php
+++ rexseo/install.inc.php
@@ -74,12 +74,15 @@ if(count($error)==0)
   // SETUP METAINFO
   //////////////////////////////////////////////////////////////////////////////
     // 4.3.x -> zusätzliches Feld "restrictions" (http://forum.redaxo.de/sutra80188.html#80188 -> erst ab metainfo r1871)
-    //a62_add_field( $title,                    $name,                     $prior, $attributes, $type, $default, $params = null,                                                                       $validate = null, $restrictions = '')
-      a62_add_field( 'RexSEO Einstellungen',    'art_rexseo_legend',       100,    '',         12,     '',       '',                                                                                   '',               '');
-      a62_add_field( 'Manuelle URL',            'art_rexseo_url',          101,    '',          1,     '',       '',                                                                                   '',               '');
-      a62_add_field( 'Manuelle Canonical URL',  'art_rexseo_canonicalurl', 102,    '',          1,     '',       '',                                                                                   '',               '');
-      a62_add_field( 'Page Title',              'art_rexseo_title',        103,    '',          1,     '',       '',                                                                                   '',               '');
-      a62_add_field( 'Google Sitemap Priority', 'art_rexseo_priority',     104,    '',          3,     '',       ':Automatisch berechnen|1.00:1.00|0.80:0.80|0.64:0.64|0.51:0.51|0.33:0.33|0.00:0.00', '',               '');
+    //a62_add_field( $title,                        $name,                      $prior, $attributes, $type, $default, $params = null,                                                                                         $validate = null, $restrictions = '')
+      a62_add_field( 'RexSEO Rewrite',              'art_rexseo_legend',        100,    '',         12,     '',       '',                                                                                                     '',               '');
+      a62_add_field( 'Custom URL',                  'art_rexseo_url',           101,    '',          1,     '',       '',                                                                                                     '',               '');
+      a62_add_field( 'Custom Canonical URL',        'art_rexseo_canonicalurl',  102,    '',          1,     '',       '',                                                                                                     '',               '');
+      a62_add_field( 'Custom Page Title',           'art_rexseo_title',         103,    '',          1,     '',       '',                                                                                                     '',               '');
+      a62_add_field( 'RexSEO Sitemap',              'art_rexseo_sitemap_legend',104,    '',         12,     '',       '',                                                                                                     '',               '');
+      a62_add_field( 'Sitemap Priority',            'art_rexseo_priority',      105,    '',          3,     '',       ':auto|1.00:1.00|0.80:0.80|0.64:0.64|0.51:0.51|0.33:0.33|0.00:0.00',                                    '',               '');
+      a62_add_field( 'Sitemap Changefreq',          'art_rexseo_changefreq',    105,    '',          3,     '',       ':auto|never:never|yearly:yearly|monthly:monthly|weekly:weekly|daily:daily|hourly:hourly|always:always','',               '');
+      a62_add_field( 'Sitemap Output',              'art_rexseo_sitemap_out',   106,    '',          3,     '',       ':auto|show:show|hide:hide',                                                                            '',               '');
   
   
   
