d0df97099c32cecbff90a542479ca424918f703a
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2011-12-29 01:29:52 +0000

    FIX: revert to default config settings..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@225 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,21 @@
 <?php
 /**
- * REXseo
- *  Based on the URL-Rewrite Addon
+ * RexSEO Addon
+ *
+ * @link http://gn2-code.de/projects/rexseo/
+ * @link https://github.com/gn2netwerk/rexseo
+ *
  * @author dh[at]gn2-netwerk[dot]de Dave Holloway
- * @author markus.staab[at]redaxo[dot]de Markus Staab
  * @author code[at]rexdev[dot]de jeandeluxe
- * @package redaxo4.2
- * @version 1.3
+ *
+ * Based on url_rewrite Addon by
+ * @author markus.staab[at]redaxo[dot]de Markus Staab
+ *
+ * @package redaxo4.2.x/4.3.x
+ * @version 1.4
  * @version svn:$Id$
  */
- 
+
 // ADDON PARAMS
 ////////////////////////////////////////////////////////////////////////////////
 $myself = 'rexseo';
@@ -50,38 +56,51 @@ require_once $myroot.'/functions/function.rexseo_helpers.inc.php';
 ////////////////////////////////////////////////////////////////////////////////
 // --- DYN
 $REX["ADDON"]["rexseo"]["settings"] = array (
-  'rexseo_version' => '1.3.223',
-  'first_run' => 0,
-  'alert_setup' => 0,
-  'install_subdir' => '',
+  'rexseo_version' => $REX['ADDON']['version'][$myself],
+  'first_run' => 1,
+  'alert_setup' => 1,
+  'install_subdir' => rexseo_subdir(),
   'url_whitespace_replace' => '-',
   'compress_pathlist' => 1,
-  'redirects_imported' => '1',
-  'def_desc' => 
+  'def_desc' =>
   array (
-    0 => 'main desc @ backupfile',
+    0 => '',
     1 => '',
+    2 => '',
+    3 => '',
+    4 => '',
+    5 => '',
+    6 => '',
+    7 => '',
+    8 => '',
   ),
-  'def_keys' => 
+  'def_keys' =>
   array (
-    0 => 'main keys @ backupfile',
+    0 => '',
     1 => '',
+    2 => '',
+    3 => '',
+    4 => '',
+    5 => '',
+    6 => '',
+    7 => '',
+    8 => '',
   ),
   'title_schema' => '%B - %S',
   'url_schema' => 'rexseo',
   'url_ending' => '.html',
   'hide_langslug' => -1,
-  'rewrite_params' => 1,
+  'rewrite_params' => 0,
   'params_starter' => '++',
   'homeurl' => 1,
   'homelang' => 0,
-  'urlencode' => 1,
+  'urlencode' => 0,
   'allow_articleid' => 0,
   'levenshtein' => 0,
-  'default_redirect_expire' => '60',
+  '301s' => array (),
   'robots' => 'User-agent: *
-Disallow:  @ backupfile',
-  'expert_settings' => 1,
+Disallow:',
+  'expert_settings' => 0,
 );
 // --- /DYN
 
@@ -105,12 +124,7 @@ if ($REX['MOD_REWRITE'] !== false)
   {
     rex_register_extension('GENERATE_FILTER', 'rexseo_fix_42x_links');
   }
-
-  // INJECT 301 URLS INTO REXSEO PATHLIST
-  //rex_register_extension('REXSEO_PATHLIST_CREATED','rexseo_inject_301');
 }
-
-//include $REX['INCLUDE_PATH'].'/addons/'.$myself.'/controller.inc.php';
 
 
 // RUN CACHER ON DB CHANGES
@@ -120,7 +134,6 @@ if ($REX['REDAXO'])
   rex_register_extension('REX_FORM_SAVED','rexseo_ht_update_callback');
   function rexseo_ht_update_callback($params)
   {
-    //FB::log($params,'REX_FORM_SAVED:');
     rexseo_htaccess_update_redirects();
   }
 }
@@ -140,10 +153,10 @@ if ($REX['REDAXO'] && $REX['MOD_REWRITE'] !== false)
   rex_register_extension('REXSEO_PATHLIST_CREATED','rexseo_auto_301');
   function rexseo_auto_301($params)
   {
-    global $REX;                                                                #FB::group('rexseo_auto_301');
+    global $REX;
 
     $diff = array();
-    $diff = array_diff(array_keys($REX['REXSEO_PRIOR_URLS']),array_keys($params['subject']['REXSEO_URLS'])); #FB::log($diff,'$diff');
+    $diff = array_diff(array_keys($REX['REXSEO_PRIOR_URLS']),array_keys($params['subject']['REXSEO_URLS']));
 
     if(is_array($diff) && count($diff)>0)
     {
@@ -155,10 +168,10 @@ if ($REX['REDAXO'] && $REX['MOD_REWRITE'] !== false)
       {
         $qry .= PHP_EOL.'(\'\', \''.$date.'\', \''.$date.'\', \''.$expire.'\', \'rexseo\', 1, \''.$url.'\', '.$REX['REXSEO_PRIOR_URLS'][$url]['id'].', '.$REX['REXSEO_PRIOR_URLS'][$url]['clang'].', 301),';
       }
-      $qry = rtrim($qry,',').';';                                               #FB::log($qry,'$qry');
+      $qry = rtrim($qry,',').';';
       $db->setQuery($qry);
       rexseo_htaccess_update_redirects();
-    }                                                                           #FB::groupEnd();
+    }
   }
 }
 
