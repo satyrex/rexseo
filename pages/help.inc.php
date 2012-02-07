b0c44dacb43959ff1e569f8507b1dfd57d49bf37
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2012-02-07 10:43:30 +0000

    log file viewer page raus..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@259 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

--- pages/help.inc.php
+++ pages/help.inc.php
@@ -42,7 +42,6 @@ $chapterpages = array (
 ''            => array('Quickstart',                   'pages/help_quickstart.textile'          ,'textile'),
 'settings'    => array('Einstellungen',                'pages/help_settings.textile'            ,'textile'),
 'troubleshoot'=> array('Problemf&auml;lle & Sonstiges','pages/help_troubleshoot.textile'        ,'textile'),
-'log'         => array('Error Log                    ','pages/rexseo.log'                       ,'txt'),
 'download'    => array('Downloads'                    ,'pages/redmine_feeds.inc.php'            ,'php'),
 'wiki'        => array('Wiki'                         ,'http://gn2-code.de/projects/rexseo/wiki','jsopenwin'),
 'changelog'   => array('Changelog'                    ,'pages/redmine_feeds.inc.php'            ,'php'),
