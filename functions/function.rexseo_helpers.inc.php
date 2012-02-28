98d8ca5e86713cb58695e3ee767ad8833ff55d87
jeandeluxe <jeandeluxe@7dd085ce-4fa9-4cde-96bc-bde5fc287a16>
2012-02-28 11:00:48 +0000

    fehlermeldung wg. fehlenden redirects block delimiters konkretisiert..
    git-svn-id: http://svn.gn2-host4.de:8080/public/rexseo/trunk@277 7dd085ce-4fa9-4cde-96bc-bde5fc287a16

--- functions/function.rexseo_helpers.inc.php
+++ functions/function.rexseo_helpers.inc.php
@@ -508,8 +508,8 @@ Pfad: "'.$ht_path.'"');
   {
     if(preg_match("@(### REXSEO REDIRECTS BLOCK.*### /REXSEO REDIRECTS BLOCK)@s",$ht_content)!=1)
       echo rex_warning('ACHTUNG: redirects konnten nicht geschrieben werden!<br />
-Bitte die .htaccess auf <a>korrektes redirects delimiter Paar</a> überprüfen.<br />
-(Siehe original RexSEO htaccess: ../addons/rexseo/install/files/.htaccess)');
+Bitte die .htaccess auf korrektes redirects delimiter Paar überprüfen.<br />
+                       (Siehe original RexSEO htaccess: <em style="color:black;">./addons/rexseo/install/files/.htaccess</em> )');
 
     $new_ht = preg_replace("@(### REXSEO REDIRECTS BLOCK.*### /REXSEO REDIRECTS BLOCK)@s", $new_redirects, $ht_content);
     return rex_put_file_contents($ht_path, $new_ht);
