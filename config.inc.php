0f303639e704fe5c4c21df8e702b12f08037467d
jdlx <github@rexdev.de>
2012-04-18 20:27:15 +0200

    version bump (lost via assume unchanged..)

--- config.inc.php
+++ config.inc.php
@@ -11,7 +11,7 @@
  * @author markus.staab[at]redaxo[dot]de Markus Staab
  *
  * @package redaxo4.3.x
- * @version 1.4.282-dev
+ * @version 1.4.283-dev
  */
 
 // ADDON PARAMS
@@ -23,7 +23,7 @@ $REX['ADDON'][$myself]['VERSION'] = array
 (
 'VERSION'      => 1,
 'MINORVERSION' => 4,
-'SUBVERSION'   => '282-dev',
+'SUBVERSION'   => '283-dev',
 );
 
 $REX['ADDON']['rxid'][$myself]        = '750';
